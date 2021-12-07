24e93509fde0c62115e5f4fdbb9b68bc746358b1
Luthfi Finn <61146151+luthfifinnkennedy@users.noreply.github.com>
2021-12-07 20:22:22 +0800

    Add files via upload

---
+++ lib/peta.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class peta extends StatelessWidget {
+  const peta({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Peta'),
+        )
+    );
+  }
+}
